1f20964d73caf0f7adcfb71dd58d57c2d537b1d4
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-02-22 20:05:13 +0100

    Add max 3 retry while there is no `choices` property in AI response

--- src/scrapper/index.ts
+++ src/scrapper/index.ts
@@ -15,23 +15,34 @@ async function scrapePageByAI(options: ScrapperOptions, pageInfos: {
   html: string;
   url: string;
 }) {
-  const response = await options.ai.completions({
-    messages: [
-      {
-        role: 'system',
-        content: 'You are a smart assistant that helps people find the information they need in an HTML document. Return the exact information that is requested in the schema, no more, no less. Pay attention to the schema and the schema descriptions.',
-      },
-      {
-        role: 'user',
-        content: `
-          - URL: ${pageInfos.url}
-          - HTML: ${pageInfos.html}
-        `,
-      }
-    ],
-    responseFormat: options.schema,
-    stream: false,
-  })
+  let response;
+  let retries = 3;
+
+  do {
+    response = await options.ai.completions({
+      messages: [
+        {
+          role: 'system',
+          content: 'You are a smart assistant that helps people find the information they need in an HTML document. Return the exact information that is requested in the schema, no more, no less. Pay attention to the schema and the schema descriptions. Stricly follow the schema descriptions. Always return valid JSON.',
+        },
+        {
+          role: 'user',
+          content: `
+            - URL: ${pageInfos.url}
+            - HTML: ${pageInfos.html}
+          `,
+        }
+      ],
+      responseFormat: options.schema,
+      stream: false,
+    });
+
+    if (response.choices) {
+      break;
+    }
+
+    retries--;
+  } while (retries > 0);
 
   return JSON.parse(response.choices[0].message.content)
 }
