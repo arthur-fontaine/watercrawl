3e2689562cca3c01e785e79c569404a60c036419
Arthur Fontaine <0arthur.fontaine@gmail.com>
2025-03-01 13:15:34 +0100

    Fix cli not executing

--- src/adapters/primary/cli/index.ts
+++ src/adapters/primary/cli/index.ts
@@ -1,8 +1,13 @@
+#!/usr/bin/env bun
+
 import { program } from 'commander'
 import { scrapeCommand } from './commands/scrape'
 import { flowCommand } from './commands/flow'
+import packageJson from '../../../../package.json'
 
 program
+  .name(packageJson.name)
+  .version(packageJson.version)
   .addCommand(scrapeCommand)
   .addCommand(flowCommand)
   .allowExcessArguments(false)
